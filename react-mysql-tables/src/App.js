234086fcfafe72df30ff49ad6cff22214052dce7
Philippe Recto <precto1285@gmal.com>
2019-05-07 15:56:04 -0700

    created Front end for database

--- react-mysql-tables/src/App.js
+++ react-mysql-tables/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    quotes: [],
+    quote: {
+      author: '',
+      quote: 0
+    }
+  }
+
+  componentDidMount() {
+    this.getQuotes();
+  }
+
+  getQuotes = _ => {
+    fetch('http://localhost:4000/quotes')
+      .then(response => response.json())
+      .then(response => this.setState({ quotes: response.data }))
+      .catch(err => console.error(err));
+  }
+
+
+  add_Quote = _ => {
+    const { quote } = this.state;
+    fetch(`http://localhost:4000/quotes/add?author=${quote.author}&quote=${quote.quote}`)
+      .then(this.getQuotes)
+      .catch(err => console.error(err))
+  }
+
+  renderQuote = ({ id, author }) => <div key={id}>{author}</div>
+
+  render() {
+    const { quotes } = this.state;
+    return (
+      <div className="App">
+        {quotes.map(this.renderQuote)}
+        <div>
+          <input value={quote.author}
+            onChange={e => this.setState({ quote: { ...quote, author: e.target.value } })} />
+          <input value={product.price}
+            onChange={e => this.setState({ quote: { ...quote, quote: e.target.value } })} />
+          <button onClick={this.addQuote}>Add Quote</button>
+        </div>
+
+      </div>
+    )
+  }
+};
 
 export default App;
